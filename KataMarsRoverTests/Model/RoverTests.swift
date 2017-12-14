d55534d1f9416a9d8c9542b98f4cd8250debd4e5
Manu Mateos <manu.mateos@tecnilogica.com>
2017-12-14 10:42:09 +0100

    .

--- KataMarsRoverTests/Model/RoverTests.swift
+++ KataMarsRoverTests/Model/RoverTests.swift
@@ -11,9 +11,12 @@ import XCTest
 
 class KataMarsRoverTests: XCTestCase {
     
+    var sut: Rover?
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = Rover()
     }
     
     override func tearDown() {
@@ -21,16 +24,43 @@ class KataMarsRoverTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSutIsNotNil() {
+        //Arrange
+        //Act
+        //Assert
+        XCTAssertNotNil(sut)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitialXPositionIs0() {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+    }
+
+    func testInitialYPositionIs0() {
+        //Arrange
+        
+        //Act
+        
+        //Assert
     }
     
+    func testInitialRoverIsAtDefaultX() {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+    }
+    
+    func testInitialRoverIsAtDefaultY() {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+    }
+
 }
